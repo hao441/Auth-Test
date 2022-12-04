70f0a1b0439dc53ddab29ea0792cfd2ae46c070c
Hao <har@mailbox.org>
2022-12-04 15:16:35 +1100

    Initial Commit

---
+++ Authentication/AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationApp.swift
+//  Authentication
+//
+//  Created by Hao on 4/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
